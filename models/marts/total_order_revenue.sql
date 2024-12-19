40851f1c3576260db644197c5fdb7838e33c9b38
Fred Benkimoun <benkimoun@maywater.ch>
2024-12-19 18:20:50 +0000

    sss

--- models/marts/total_order_revenue.sql
+++ models/marts/total_order_revenue.sql
@@ -1,12 +1,9 @@
-with orders as (
-    select * from fct_orders
-), 
-
+with 
 agg as (
     select 
         ordered_at,
         sum(order_total) as order_revenue 
-    from orders 
+    from stg_orders 
     group by 1
 )
 
