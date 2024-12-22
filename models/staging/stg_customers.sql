7148c3a32198959d480291d31ccaf4d281b738b1
Fred Benkimoun <benkimoun@maywater.ch>
2024-12-22 17:46:10 +0000

    rrrrtttt

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,4 +1,9 @@
 with stg_customers as
-(select * from {{source('jaffle','customers')}})
+(select 
+
+ id as customer_id
+,name as customer_name
+
+ from {{source('jaffle','customers')}})
 
 select * from stg_customers
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,14 @@
 with stg_orders as
-(select * from {{ source('jaffle', 'orders') }})
+(select 
+
+ id as order_id
+,customer as customer_id
+,ordered_at
+,store_id
+,subtotal
+,tax_paid
+,order_total as order_amount
+
+from {{ source('jaffle', 'orders') }})
 
 select * from stg_orders
