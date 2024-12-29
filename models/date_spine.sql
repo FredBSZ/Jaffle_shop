e5cd88ab23fce2381cab4f2a891b4fba6d5f305e
Fred Benkimoun <benkimoun@maywater.ch>
2024-12-29 16:25:27 +0000

    EEE

---
+++ models/date_spine.sql
@@ -0,0 +1,10 @@
+with date_spine as 
+
+({{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2024-01-01' as date)",
+    end_date="cast('2025-01-01' as date)"
+   )
+}})
+
+select * from date_spine
